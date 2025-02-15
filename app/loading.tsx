eedeb474ef494a62193d3e3cb1c0bcd49d7da39e
Pradum <satendarpradum@gmail.com>
2025-02-15 13:59:03 +0530

    Brqn

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,6 +2,7 @@ export default function loading(){
     return (
         <div className="flex flex-col justify-center h-screen">
           <div className="flex justify-center">
+            
             <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
           </div>
         </div>
