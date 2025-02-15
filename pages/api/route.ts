514808f59f5447f2e0909412e5080107735aa203
Pradum <satendarpradum@gmail.com>
2025-02-15 13:17:18 +0530

    init Simple template

--- pages/api/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
-
-export default  function Get(req:NextApiRequest,res:NextApiResponse){
-    return res.status(200).json({
-        username:'Pradum',
-        email:'satendarpradum@gmail.com'
-    })
-}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,10 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black:{
+          DEFAULT:'#000',
+          100:'#000319'
+        }
       },
     },
   },
